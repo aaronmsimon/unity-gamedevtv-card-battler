b5ebfab43822cb72b87638f0ad9356dbd5ba7dec
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2026-05-30 10:57:36 -0700

    Card Hovering View (1.9)

--- Card Battler/Assets/Scripts/Card.cs
+++ Card Battler/Assets/Scripts/Card.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using TMPro;
 
 public class Card : MonoBehaviour
@@ -10,8 +11,26 @@ public class Card : MonoBehaviour
 
     [SerializeField] private CardData tempCardData;
 
+    [SerializeField] private float hoverScale = 2f;
+    [SerializeField] private float hoverOffset = 2f;
+
+    private Vector3 originalScale;
+    private Vector3 originalPos;
+
+    private SortingGroup sortingGroup;
+    private int originalSortOrder;
+
+    private void Awake()
+    {
+        sortingGroup = GetComponent<SortingGroup>();
+    }
+
     private void Start()
     {
+        originalScale = transform.localScale;
+        originalPos = transform.localPosition;
+        originalSortOrder = sortingGroup.sortingOrder;
+
         LoadCardData(tempCardData);
     }
 
@@ -21,5 +40,21 @@ public class Card : MonoBehaviour
         cardNameText.text = cardData.cardName;
         descriptionText.text = cardData.description;
         actionsText.text = cardData.actionCost.ToString();
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Mouse entered");
+        transform.localScale = originalScale * hoverScale;
+        transform.localPosition += new Vector3(0f, hoverOffset, 0f);
+        sortingGroup.sortingOrder += 1;
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Mouse exited");
+        transform.localScale = originalScale;
+        transform.localPosition = originalPos;
+        sortingGroup.sortingOrder = originalSortOrder;
     }
 }
