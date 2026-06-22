8ce51e9c9d39a1f1aa53fda02520511b92e6c67d
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2026-06-21 23:14:09 -0700

    Dragging & Snapping (1.12)

--- Card Battler/Assets/Scripts/Card.cs
+++ Card Battler/Assets/Scripts/Card.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class Card : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class Card : MonoBehaviour
 
     private SortingGroup sortingGroup;
     private int originalSortOrder;
+
+    private static bool isBeingDragged = false; // static so in case raycast goes through one card to another
 
     private void Awake()
     {
@@ -40,6 +43,8 @@ public class Card : MonoBehaviour
 
     public void OnMouseEnter()
     {
+        if (isBeingDragged) return;
+
         Debug.Log("Mouse entered");
         transform.localScale = originalScale * hoverScale;
         transform.localPosition += new Vector3(0f, hoverOffset, 0f);
@@ -48,9 +53,33 @@ public class Card : MonoBehaviour
 
     public void OnMouseExit()
     {
+        if (isBeingDragged) return;
+
         Debug.Log("Mouse exited");
         transform.localScale = originalScale;
         transform.localPosition = originalPos;
         sortingGroup.sortingOrder = originalSortOrder;
+    }
+
+    private void OnMouseDrag()
+    {
+        isBeingDragged = true;
+        gameObject.transform.position = GetMousePosition();
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        mousePosition.z = transform.position.z - Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Mouse up");
+        isBeingDragged = false;
+        transform.localScale = originalScale;
+        transform.localPosition = originalPos;
+        sortingGroup.sortingOrder = originalSortOrder;        
     }
 }
