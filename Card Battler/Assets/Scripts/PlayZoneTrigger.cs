20014d7d028033199792391e5733372f9f7ab8c6
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2026-06-23 19:03:59 -0700

    Playzone (1.13)
    Completed the lesson. Previously, the hover over a card caused it to flash back-and-forth doing the grow and shrink methods without actually clicking or moving the mouse. This continued, but sort of just stopped. I did also change some of the positions of the colliders, but it still happened one more time and now it works fine so idk

--- Card Battler/Assets/Scripts/PlayZoneTrigger.cs
+++ Card Battler/Assets/Scripts/PlayZoneTrigger.cs
@@ -4,10 +4,17 @@ public class PlayZoneTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("playzone trigger");
         if (collision.TryGetComponent(out Card card))
         {
-            Debug.Log("CARD");
+            Debug.Log("CARD entered");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Card card))
+        {
+            Debug.Log("CARD left");
         }
     }
 }
